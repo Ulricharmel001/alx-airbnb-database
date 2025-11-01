c8176b9aa255752e4e6f5f51ead45567db01b808
Ulrich Armel <ulrichsgraphixs@gmail.com>
2025-11-01 06:33:09 +0100

    practice writing complete query to retrive data

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,8 +1,4 @@
--- =========================================
--- joins_queries.sql
--- =========================================
-
--- 1) INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+-- INNER JOIN: get all bookings with the users who made them
 SELECT 
     u.first_name,
     u.last_name,
@@ -10,43 +6,37 @@ SELECT
     b.start_date,
     b.end_date,
     b.total_price
-FROM "User" AS u
-INNER JOIN Booking AS b ON u.user_id = b.user_id;
+FROM `User` AS u
+INNER JOIN Booking AS b ON u.user_id = b.user_id
+ORDER BY b.booking_id;
 
--- 2) LEFT JOIN: Retrieve all properties and their reviews, including properties with no reviews
+-- LEFT JOIN: get all properties with their reviews (include properties without reviews)
 SELECT 
     p.name AS property_name,
     r.rating,
     r.comment
 FROM Property AS p
-LEFT JOIN Review AS r ON p.property_id = r.property_id;
+LEFT JOIN Review AS r ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
--- 3) FULL OUTER JOIN: Retrieve all users and all bookings, even if unmatched
--- PostgreSQL / SQL Server version (supports FULL OUTER JOIN)
+-- FULL OUTER JOIN workaround in MySQL: get all users and all bookings
 SELECT 
     u.first_name,
     u.last_name,
     b.booking_id,
     b.start_date,
     b.end_date
-FROM "User" AS u
-FULL OUTER JOIN Booking AS b ON u.user_id = b.user_id;
-
--- 3b) MySQL version (if FULL OUTER JOIN not supported)
-SELECT 
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.start_date,
-    b.end_date
-FROM "User" AS u
+FROM `User` AS u
 LEFT JOIN Booking AS b ON u.user_id = b.user_id
+
 UNION
+
 SELECT 
     u.first_name,
     u.last_name,
     b.booking_id,
     b.start_date,
     b.end_date
-FROM "User" AS u
-RIGHT JOIN Booking AS b ON u.user_id = b.user_id;
+FROM `User` AS u
+RIGHT JOIN Booking AS b ON u.user_id = b.user_id
+ORDER BY booking_id;
