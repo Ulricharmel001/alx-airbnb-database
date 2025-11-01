186c48420f5077b638eed0d24f5332ed8459a7cc
Ulrich Armel <ulrichsgraphixs@gmail.com>
2025-11-01 07:07:39 +0100

    Added aggregation_and_window_function.sql rank properties by  total booking using ROW_NUMBER()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,26 +1,21 @@
---- total booking per user 
-SELECT user_id, COUNT(*) AS total_bookings
-FROM Booking
-GROUP BY user_id
-ORDER BY total_bookings DESC;
-
---- rant property by total booking 
-
-SELECT property_id
+-- Rank properties by total bookings using ROW_NUMBER()
+SELECT 
+    property_id,
     COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
 FROM Booking
 GROUP BY property_id
 ORDER BY booking_rank;
 
 
-
-
--- Indexes to improve query performance
-CREATE INDEX idx_booking_user_id ON Booking(user_id);
-CREATE INDEX idx_booking_property_id ON Booking(property_id);
-CREATE INDEX idx_review_property_id ON Review(property_id);
-CREATE INDEX idx_property_location ON Property(location);
+-- Rank properties by total bookings
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM Booking
+GROUP BY property_id
+ORDER BY booking_rank;
 
 
 
